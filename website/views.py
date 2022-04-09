edd9fc4066412f55b73edd5eaf11809b091b9b0b
boluwatifeh <tifeasypeasy@gmail.com>
2022-04-09 16:24:45 +0100

    update blueprints

--- website/views.py
+++ website/views.py
@@ -1,5 +1,8 @@
-from flask import Blueprint
+import json
+from flask import Blueprint, jsonify
 
 views = Blueprint('views', __name__)
 
 @views.route("/")
+def index():
+    return jsonify({"message": "welcome to the home page"})
