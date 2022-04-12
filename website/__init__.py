18c7926e89f57956f001e98c522b8a1d4e66f524
boluwatifeh <tifeasypeasy@gmail.com>
2022-04-12 23:47:34 +0100

    setup database model

--- website/__init__.py
+++ website/__init__.py
@@ -1,13 +1,29 @@
 from flask import Flask, url_for
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'HHEldkj 033rk ldk 0lkle'
-
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{DB_NAME}"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/auth")
 
+    from .models import User, Note
+
+    create_db(app)
+
     return app 
+
+def create_db(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Database created!')
