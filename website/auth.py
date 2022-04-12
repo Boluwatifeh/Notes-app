679d2a5175f7cc5b9608400e23cadca2a053891d
boluwatifeh <tifeasypeasy@gmail.com>
2022-04-12 23:16:16 +0100

    implement flashing of messages

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,12 @@
-from flask import Blueprint, render_template
+import re
+from flask import Blueprint, render_template, request, flash, get_flashed_messages
 
 auth = Blueprint('auth', __name__)
 
 
 @auth.route("/login", methods=["POST", "GET"])
 def login():
+    
     return render_template("login.html")
 
 
@@ -15,6 +17,21 @@ def logout():
 
 @auth.route("/sign-up", methods=["POST", "GET"])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstname = request.form.get('firstname')
+        password = request.form.get('password1')
+        confirm_password = request.form.get('password2')  
+
+        if len(firstname) < 2:
+            flash('Name should be greater than 1 character', category='error')
+        elif len(password) < 6:
+            flash('Password should be greater than 6 characters', category='error')
+        elif password != confirm_password:
+            flash('Passwords do not match, try again', category='error')
+        else:
+            flash('Account created!', category='success')   
+
     return render_template("sign_up.html")
 
 
